4bbf9e81302e2cebcd2a2943f0d63f0ccb2c733e
Heaven1619 <heavenr1619@gmail.com>
2016-01-11 23:46:00 +0330

    Update code.js
    I've forgot to update code.js in previous commit!

--- js/code.js
+++ js/code.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  
+  $('#Add').on('click',Addrow);
+
 });
+
+function Addrow(){
+  var number = ($('tbody tr').length)+1;
+  var tr = '          <tr>'+
+              '<td>'+number+'</td>'+
+              '<td><input type="text" placeholder="Student Name" class="form-control name"></td>'+
+              '<td><input type="number" placeholder="Score" class="form-control score"></td>'+
+              '<td><input type="number" placeholder="Forgiveness" class="form-control score"></td>'+
+              '<td><span class="final"></span></td>'+
+              '<td><span class="status"></span></td>'+
+              '<td><input type="button" class="btn btn-primary btn-sm remove" value="Remove"></td>'+
+            '</tr>'
+  $('tbody').append(tr);
+}
